3bbf71cbb86df6fe2e9bf78ab114c3e55bab4ce7
tsukigahara <satsuki.tksm@gmail.com>
2023-03-09 10:57:04 +0100

    added Apart.Controller, Img.Controller, api routes

---
+++ progetto-finale/app/Http/Controllers/ApartmentController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Apartment;
+use App\Models\Service;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+
+class ApartmentController extends Controller
+{
+    // show all apartments with paginations (12 elementi alla volta)
+    public function index()
+    {
+        $apartments = Apartment::paginate(12);
+        $apartments->load('sponsorships');
+
+        return response()->json([
+            "success" => true,
+            "response" => $apartments,
+        ]);
+    }
+
+    // show single apartment by id with relations
+    public function show(Apartment $apartment)
+    {
+        $apartment->load('user');
+        $apartment->load('images');
+        $apartment->load('services');
+        $apartment->load('sponsorships');
+
+        return response()->json([
+            "success" => true,
+            "response" => [
+                "data" => [
+                    "apartments" => $apartment
+                ],
+            ]
+        ]);
+    }
+
+    // delete apartment (and it will delete all relations)
+    public function destroy(Apartment $apartment)
+    {
+        $apartment->user()->dissociate();
+        $apartment->views()->delete();
+        $apartment->messages()->delete();
+        $apartment->images()->delete();
+        $apartment->sponsorships()->sync([]);
+        $apartment->services()->sync([]);
+        $apartment->delete();
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
+
+    // create apartment (for create form page)
+    public function create()
+    {
+        $services = Service::all();
+
+        return response()->json([
+            "success" => true,
+            "response" => [
+                "data" => [
+                    "services" => $services
+                ],
+            ]
+        ]);
+    }
+
+    //  store apartment
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|min:0|max:128',
+            'rooms' => 'required|integer|min:0',
+            'beds' => 'required|integer|min:0',
+            'bathrooms' => 'required|integer|min:0',
+            'square_meters' => 'required|integer|min:0',
+            'address' => 'required|string|min:0|max:128',
+            'latitude' => 'required|string|min:0|max:16',
+            'longitude' => 'required|string|min:0|max:16',
+            'main_image' => 'required|string|min:0|max:128',
+            'visible' => 'required|boolean',
+            'price' => 'required|integer|min:0',
+            'description' => 'string',
+            'services_id' => 'nullable|array',
+            'user_id' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $data = $request->all();
+
+        $apartment = Apartment::make($data);
+
+        // one to many
+        $user = User::find($data['user_id']);
+        $apartment->user()->associate($user);
+        $apartment->save();
+
+        // many to many
+        if (array_key_exists('services_id', $data)) {
+            $services = Service::find($data['services_id']);
+            $apartment->services()->sync($services);
+        }
+
+        return response()->json([
+            'success' => true,
+            'response' => $apartment,
+            'data' => $request->all()
+        ]);
+    }
+
+    public function edit()
+    {
+        // da fare
+    }
+
+    public function update()
+    {
+        // da fare
+    }
+}
--- progetto-finale/routes/api.php
+++ progetto-finale/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ApartmentController;
+use App\Http\Controllers\ImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,36 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/////////////////////////////////////////////////////////////////////////////////
+///////////////PROTECTED ROUTES /////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+Route::middleware('auth')->group(function () {
+
+    ///////////APARTMENT CONTROLLER /////////////
+    Route::resource('/v1/apartments', ApartmentController::class)->except(['update', 'show', 'index']);
+    Route::post('/v1/apartments/{apartment}', [ApartmentController::class, 'update'])->name('apartments.update');
+
+    ///////////IMAGE CONTROLLER////////////////
+    Route::delete('/v1/apartments/image/{image}', [ImageController::class, 'deleteImage'])->name('image.delete');
+    Route::post('/v1/apartments/image/store', [ImageController::class, 'storeImage'])->name('image.store');
+});
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+///////////////PUBLIC ROUTES///////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+///////////APARTMENT CONTROLLER /////////////
+Route::get('/v1/apartments', [ApartmentController::class, 'index'])->name('apartments.index');
+Route::get('/v1/apartments/{apartment}', [ApartmentController::class, 'show'])->name('apartments.show');
+// Route::delete('/v1/apartments/{apartment}', [ApartmentController::class, 'destroy'])->name('apartments.destroy');
+// Route::get('/v1/apartments/create', [ApartmentController::class, 'create'])->name('apartments.create');
+// Route::post('/v1/apartments/store', [ApartmentController::class, 'store'])->name('apartments.store');
+
+
+///////////IMAGE CONTROLLER////////////////
+Route::get('/v1/apartments/image/{apartment_id}', [ImageController::class, 'showImages'])->name('apartment.image');
